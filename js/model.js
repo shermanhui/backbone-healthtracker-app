5a23d3a636e4de7afde24af7bd31728793fd9fba
shui91 <sherman.sy.hui@gmail.com>
2015-11-13 10:04:51 -0800

    chore: added comments

--- js/model.js
+++ js/model.js
@@ -168,13 +168,16 @@ app.AppView = Backbone.View.extend({
 
 			this.userSearch = "https://api.nutritionix.com/v1_1/search/"+ this.$inputURL +"?results=0:10&fields=item_name,brand_name,item_id,nf_calories&appId=cd0bcc78&appKey=9aec12536b3cf72ef688e2489200ba31";
 
-			app.foods.url = this.userSearch; // should i use this or var?
+			app.foods.url = this.userSearch; // use 'this' or var?
+
+
 
 			app.foods.fetch().then(function(){
-				app.FoodListView = new app.FoodListView({collection: app.foods});
-				app.FoodListView.render();
-			});
+				var FoodListView = new app.FoodListView({collection: app.foods});
+				FoodListView.render();
 
+			});
+			// app.foods.reset();
 			this.$input.val(''); //clears input after Enter
 		}
 	}
